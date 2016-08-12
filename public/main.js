639a21ee541496e5c8ec4e1448df2138ef801a5a
Chie <akiba.chie@gmail.com>
2016-08-12 15:44:23 -0700

    Made comments for the next step

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //global variables
     var socket = io();
     var input = $('input');
     var messages = $('#messages');
@@ -6,6 +7,12 @@ $(document).ready(function () {
     var addMessage = function (message) {
         messages.append('<div>' + message + '</div>');
     };
+
+    //When user connects and/or disconnects show that the user connected/disconnected
+
+    //Show who is online
+
+    //When user is typing, show that the user is typing
 
     input.on('keydown', function (event) {
         if (event.keyCode != 13) {
