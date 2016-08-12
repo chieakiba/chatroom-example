69d92628cfb8a3de579df3d0627f26f3038e2245
Chie <akiba.chie@gmail.com>
2016-08-12 15:06:19 -0700

    The original chatroom codes. Should just send messages

--- public/main.js
+++ public/main.js
@@ -1,66 +1,21 @@
 $(document).ready(function () {
-    var username = prompt('Enter your username');
     var socket = io();
     var input = $('input');
     var messages = $('#messages');
 
     var addMessage = function (message) {
-        if (typeof message == 'object') {
-            console.log('message', message);
-
-            messages.append('<div>' + message.username + ": " + message.message + '</div>');
-        } else {
-            console.log('message', message);
-
-            messages.append('<div>' + username + ": " + message + '</div>');
-        }
-
+        messages.append('<div>' + message + '</div>');
     };
 
-    var userConnected = function (username) {
-        messages.html('<div>' + username + ' has connected!</div>');
-    };
-
-    var userDisconnected = function (username) {
-        messages.html('<div>' + username + ' has disconnected!</div>');
-    };
-
-    var room = 'General';
-
-    socket.on('connect', function () {
-        socket.emit('userConnect', username);
-        console.log('User joined', room);
-    });
-
-    socket.on('users', function (usernames) {
-        console.log('Username(s) joined the chat', usernames);
-        $('.online').text(usernames.length + " online");
-        socket.emit('online', usernames.length);
-    });
-
-    socket.on('online', function (length) {
-        alert(length);
-    });
     input.on('keydown', function (event) {
         if (event.keyCode != 13) {
             return;
         }
 
         var message = input.val();
+        socket.emit('message', message);
         addMessage(message);
-        socket.emit('message', {
-            message: message,
-            username: username
-        });
         input.val('');
-
     });
     socket.on('message', addMessage);
-
-    socket.on('disconnect', function (room) {
-        console.log('User left', room);
-        socket.emit(username, ' left ', room);
-        usernames.splice(usernames.indexOf(username), 1)
-        socket.on('username', userDisconnected);
-    });
 });
--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+var express = require('express');
+
+var app = express();
+app.use(express.static('public'));
+
 var socket_io = require('socket.io');
 var http = require('http');
 var express = require('express');
@@ -8,31 +13,13 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
-var usernames = [];
-
 io.on('connection', function (socket) {
     console.log('Client connected');
 
-    socket.on('connect', function (room) {
-        console.log('User joined', room);
-        socket.broadcast.emit('User joined ', room);
-    });
-
-    socket.on('userConnect', function (username) {
-        usernames.push(username);
-        //        console.log('User connected', username);
-        socket.broadcast.emit('users', usernames);
-    });
-
-    socket.on('message', function (message) {
+    socket.on('message', function(message) {
         console.log('Received message:', message);
         socket.broadcast.emit('message', message);
     });
-    //remove usernames from array (array manipulation)
-    socket.on('disconnect', function (room) {
-        console.log('User left', room);
-        socket.broadcast.emit('User left ', room);
-    });
 });
 
-server.listen(process.env.PORT || 8080);
+server.listen(8080);
