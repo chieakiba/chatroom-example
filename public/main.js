0522c5a650fd8a26d3fdd26bc1ca83237ebcc434
Chie <akiba.chie@gmail.com>
2016-08-12 15:51:56 -0700

    Added username variable and usernames array so the users can enter their names into the prompt and their names will be pushed into the usernames array

--- public/main.js
+++ public/main.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     var socket = io();
     var input = $('input');
     var messages = $('#messages');
+    var username = prompt('Please enter your name');
+    var usernames = [];
 
     var addMessage = function (message) {
         messages.append('<div>' + message + '</div>');
@@ -11,6 +13,9 @@ $(document).ready(function () {
     //When user connects and/or disconnects show that the user connected/disconnected
 
     //Show who is online
+
+    //Push users into the usernames array as they enter their names into the prompt
+    usernames.push(username);
 
     //When user is typing, show that the user is typing
 
