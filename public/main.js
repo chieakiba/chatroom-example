e81f46f57d1d7f9aadcb4af53489320ffa61c721
Chie <akiba.chie@gmail.com>
2016-08-13 13:41:32 -0700

    Test 6 - seeing if this works now

--- public/main.js
+++ public/main.js
@@ -9,15 +9,10 @@ $(document).ready(function () {
     var messages = $('#messages');
     var username = prompt('Please enter your name');
     var allTheUsers = [];
+    var message = input.val();
 
-    //Message object literal
-    var messageObject = {
-        username: username,
-        message: message
-    };
-
-    var addMessage = function (messageObject) {
-        messages.append('<div>' + messageObject.username + ':' + messageObject.message + '</div>');
+    var addMessage = function (username, message) {
+        messages.append('<div>' + username + ':' + message + '</div>');
     };
 
     var userDisconnected = function (username) {
@@ -70,7 +65,12 @@ $(document).ready(function () {
             return;
         }
 
-        var message = input.val();
+
+        //Message object literal
+        var messageObject = {
+            username: username,
+            message: message
+        };
 
         //Emit socket to server side so when user sends a message, the user will know who sent that message
         socket.emit('message', messageObject);
