c3d1df8c1df8abc8f2135ee2e3bd7b7d872f760b
Chie <akiba.chie@gmail.com>
2016-08-15 15:45:21 -0700

    Finally working the way I want it to

--- public/main.js
+++ public/main.js
@@ -84,7 +84,7 @@ $(document).ready(function () {
             typing: typing
         });
         clearTimeout(timeout);
-        timeout = setTimeout(timeoutFunction, 3000);
+        timeout = setTimeout(timeoutFunction, 1000);
     });
 
     socket.on('typing', function (data) {
